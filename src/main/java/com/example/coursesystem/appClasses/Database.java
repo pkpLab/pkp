a4d76c2ab575dc0342e78111e03e69df4e18ece2
Žygimantas <zygimantas877@gmail.com>
2023-04-12 00:15:39 +0300

    issue#-34-sukurtos pagalbinės funkcijos, SQL užklausos ir duomenų struktūros, skirtos ištraukti iš DB ir suformatuoti duomenis, kurie paskui bus pateikti sąraše.

--- src/main/java/com/example/coursesystem/appClasses/Database.java
+++ src/main/java/com/example/coursesystem/appClasses/Database.java
@@ -1,5 +1,8 @@
 package com.example.coursesystem.appClasses;
 
+import com.example.coursesystem.dataStructures.Course;
+import com.example.coursesystem.dataStructures.File;
+import com.example.coursesystem.dataStructures.Folder;
 import com.example.coursesystem.dataStructures.User;
 import org.apache.commons.dbcp.BasicDataSource;
 
@@ -7,6 +10,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Database {
 
@@ -103,5 +108,111 @@ public class Database {
             closeConnection(con);
         }
     }
+
+    public List<Course> getManagedCourses(int user_id) throws SQLException {
+        Connection con = ds.getConnection();
+        try {
+            List<Course> courses = new ArrayList<Course>();
+
+            PreparedStatement stmt = con.prepareStatement("SELECT DISTINCT c.course_id, c.creator_id, c.name, c.description " +
+                    "FROM courses c, user_managed_courses uc " +
+                    "WHERE ((uc.course_id = c.course_id AND uc.user_id = ?) OR c.creator_id = ?);");
+            stmt.setInt(1, user_id);
+            stmt.setInt(2, user_id);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Course course = new Course();
+                course.setCourse_id(rs.getInt(1));
+                course.setCreator_id(rs.getInt(2));
+                course.setCourseName(rs.getString(3));
+                course.setDescription(rs.getString(4));
+                courses.add(course);
+            }
+
+            return courses;
+        } finally {
+            closeConnection(con);
+        }
+    }
+
+    public List<Folder> getFoldersForParent(int course_id, int parent_id) throws SQLException {
+        Connection con = ds.getConnection();
+        try {
+            List<Folder> folders = new ArrayList<Folder>();
+
+            String sql = "SELECT folder_id, course_id, name, parent_id " +
+                    "FROM folders " +
+                    "WHERE course_id = ? ";
+            if (parent_id == 0) {
+                sql += "AND parent_id is null";
+            } else {
+                sql += "AND parent_id = ?";
+            }
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, course_id);
+            if (parent_id != 0) {
+                stmt.setInt(2, parent_id);
+            }
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Folder folder = new Folder();
+                folder.setFolder_id(rs.getInt(1));
+                folder.setCourse_id(rs.getInt(2));
+                folder.setFolderName(rs.getString(3));
+                folder.setParent_id(rs.getInt(4));
+                folders.add(folder);
+            }
+
+            return folders;
+        } finally {
+            closeConnection(con);
+        }
+    }
+
+    public List<File> getFilesForFolder(int folder_id) throws SQLException {
+        Connection con = ds.getConnection();
+        try {
+            List<File> files = new ArrayList<File>();
+
+            PreparedStatement stmt = con.prepareStatement("SELECT file_id, folder_id, name, size " +
+                    "FROM files " +
+                    "WHERE folder_id = ?");
+            stmt.setInt(1, folder_id);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                File file = new File();
+                file.setFile_id(rs.getInt(1));
+                file.setFolder_id(rs.getInt(2));
+                file.setFileName(rs.getString(3));
+                file.setFileSize(rs.getInt(4));
+                files.add(file);
+            }
+
+            return files;
+        } finally {
+            closeConnection(con);
+        }
+    }
+
+    public boolean checkIfUserIsCreator(int user_id, int course_id) throws SQLException {
+        Connection con = ds.getConnection();
+        try {
+            PreparedStatement stmt = con.prepareStatement("SELECT 1 FROM courses WHERE creator_id = ? AND course_id = ?");
+            stmt.setInt(1, user_id);
+            stmt.setInt(2, course_id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return true;
+            } else {
+                return false;
+            }
+        } finally {
+            closeConnection(con);
+        }
+    }
+
+
+
+
 }
 
--- src/main/java/com/example/coursesystem/fxControllers/ManagedCoursesController.java
+++ src/main/java/com/example/coursesystem/fxControllers/ManagedCoursesController.java
@@ -1,10 +1,16 @@
 package com.example.coursesystem.fxControllers;
 
+import com.example.coursesystem.appClasses.CourseTree;
+import com.example.coursesystem.appClasses.CurrentUser;
+import com.example.coursesystem.appClasses.Database;
+import com.example.coursesystem.dataStructures.Course;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
+
+import java.sql.SQLException;
 
 public class ManagedCoursesController {
     public TreeTableView twManagedCourses;
@@ -29,4 +35,27 @@ public class ManagedCoursesController {
 
     public void refreshCourses(ActionEvent actionEvent) {
     }
+
+    private CourseTree managedCourses;
+    private Course selectedCourse;
+
+
+    private void toggleButtons() {
+        if (selectedCourse != null) {
+            btnEditCourse.setDisable(false);
+            try {
+                if (Database.getInstance().checkIfUserIsCreator(CurrentUser.getUserId(), selectedCourse.getCourse_id())) {
+                    btnEditRights.setDisable(false);
+                    btnRemoveCourse.setDisable(false);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            btnEditCourse.setDisable(true);
+        }
+    }
+
+
+
 }
