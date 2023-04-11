beb3525de8bebe35fa6d7607531be6ca8d363eae
vytas <vytasstro@gmail.com>
2023-04-11 20:10:23 +0300

    Main View creation

---
+++ src/main/java/com/example/coursesystem/fxControllers/MainController.java
@@ -0,0 +1,18 @@
+package com.example.coursesystem.fxControllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    public MainController() {
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    public void initData() {
+    }
+}
