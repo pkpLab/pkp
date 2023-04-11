94f8c6e78a8d6c23098b4d25b43f74930ae3fe20
Žygimantas <zygimantas877@gmail.com>
2023-04-11 23:52:39 +0300

    issue#-32-sukurtas UI/FXML View

---
+++ src/main/java/com/example/coursesystem/fxControllers/ManagedCoursesController.java
@@ -0,0 +1,32 @@
+package com.example.coursesystem.fxControllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
+
+public class ManagedCoursesController {
+    public TreeTableView twManagedCourses;
+    public TreeTableColumn colCourse;
+    public TextField txtEnrolledCount;
+    public TextField txtEditRightCount;
+    public Button btnEditRights;
+    public Button btnRemoveCourse;
+    public Button btnEditCourse;
+
+    public void editUserRights(ActionEvent actionEvent) {
+    }
+
+    public void removeCourse(ActionEvent actionEvent) {
+    }
+
+    public void editCourse(ActionEvent actionEvent) {
+    }
+
+    public void createCourse(ActionEvent actionEvent) {
+    }
+
+    public void refreshCourses(ActionEvent actionEvent) {
+    }
+}
